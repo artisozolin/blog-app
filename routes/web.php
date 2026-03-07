adbd5210845c7c359b13fdd454db619eabfce68b
artisozolin <artisozolins125@gmail.com>
2026-03-07 12:23:28 +0200

    BLOG-9 - Add login functionality

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BlogDisplayPageController;
 use App\Http\Controllers\BlogHomePageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BlogHomePageController::class, 'index'])->name('blog.index');;
 Route::get('/blog/{slug}', [BlogDisplayPageController::class, 'show'])->name('blog.show');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
