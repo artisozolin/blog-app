8c51bee0845f441102c9bee5e28f04f82ac97821
artisozolin <artisozolins125@gmail.com>
2026-03-15 13:30:59 +0200

    BLOG-17 - refactor auth controller and routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function store(Request $request)
     {
         $credentials = $request->validate([
             'username' => 'required|string',
@@ -26,7 +26,7 @@ class AuthController extends Controller
         ]);
     }
 
-    public function logout(Request $request)
+    public function destroy(Request $request)
     {
         Auth::logout();
         $request->session()->invalidate();
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BlogHomePageController::class, 'index'])->name('blog.index');;
 Route::get('/blog/{slug}', [BlogDisplayPageController::class, 'show'])->name('blog.show');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/login', [AuthController::class, 'store'])->name('login');
+Route::post('/logout', [AuthController::class, 'destroy'])->name('logout');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store')->middleware('auth');
 Route::put('/posts/{post}', [PostController::class, 'update']);
 Route::delete('/posts/{post}', [PostController::class, 'destroy']);
